ae752468d354ffe2703b583d9b43b55d1ccba849
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-06-14 15:46:33 -0700

    cleaned code and added info

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,19 @@ function Player(finalScore){
   this.playerTempScore = 0;
   this.finalScore = 0;
 };
+//round starter
 round = 1;
+//gets temp score so you can add then clear out array
 tempScore = [];
 var playerOne = new Player();
 var playerTwo = new Player();
-
+//calculate temp score
 function calculate(array){
   tempScore.forEach(function(score){
     playerOne.finalScore += score
   });
 };
-
+//calculate temp score for player2
 function calculate2(array){
   tempScore.forEach(function(score){
     playerTwo.finalScore += score
@@ -22,55 +24,45 @@ function calculate2(array){
 
 function pushTemp(score){
   if(score !== 1)
-    tempScore.push(score)
+  tempScore.push(score)
   else{
     tempScore = [];
   };
 };
 
-alert
 function switchUser(){
   if(roll === 1){
-  alert("Crap, you just rolled 1. Switch player.")
-  // $("#roll1").hide()
-  // $("#roll2").hide()
-// } else if(roll !== 1){
-//   // $("#roll1").show()
-//   // $("#roll2").show()
-
-}
+    alert("Crap, you just rolled 1. Switch player.")
+  }
 };
 
- Player.prototype.win = function(){
-   if(this.finalScore >=100){
-      location.reload();
+Player.prototype.win = function(){
+  if(this.finalScore >=100){
+    location.reload();
+    alert("YOU WIN!")
+  }
+};
 
+function bonusRound(){
+  if(round === 7 && roll === 1){
+    tempScore = [];
+  }
+  else if (round === 7 ){
+    roll *= 2;
+  }
+};
 
-   alert("YOU WIN!")
- }
- };
+function bonusText(){
+  if(round !== 7){
+    $(".bonusRound").hide();
+  } else {
+    $(".bonusRound").show();
 
- function bonusRound(){
-   if(round === 7 && roll === 1){
-      tempScore = [];
-   }
-   else if (round === 7 ){
-     roll *= 2;
-    //  $(".bonusRound").text("BONUS ROUND!!!!");
-      }
- };
-
- function bonusText(){
-   if(round !== 7){
-     $(".bonusRound").hide();
-   } else {
-     $(".bonusRound").show();
-
-   }
- };
+  }
+};
 
 Player.prototype.takeScore = function(takeScore){
-    this.finalScore.push(takeScore);
+  this.finalScore.push(takeScore);
 };
 
 Player.prototype.resetArray = function(){
@@ -80,28 +72,28 @@ Player.prototype.resetArray = function(){
 // user interface
 $(function(){
   $(".round").text(round);
-    bonusText();
-//Click to roll Dice for Player 1
+  bonusText();
+  //Click to roll Dice for Player 1
   $("#roll1").click(function(){
-     arr = [1,2,3,4,5,6];
-     roll = arr[Math.floor(Math.random()*arr.length)];
+    arr = [1,2,3,4,5,6];
+    roll = arr[Math.floor(Math.random()*arr.length)];
+    //will run to clear out array if roll is 1
     switchUser();
+    //to add bonus round if at 7th round
     bonusRound();
-    bonusText();
     playerOne.playerPrevious.push(roll)
     playerOne.playerTempScore = 0;
     playerOne.playerPrevious.forEach(function(currentScore){
-    playerOne.playerTempScore += currentScore;
+      playerOne.playerTempScore += currentScore;
     });
-
-    //debugger;
+    //creates temp score so user can see how much they have so far
     var current = roll;
     for (var i = 0; i<tempScore.length; i+=1){
       current += tempScore[i];
     };
-
+    //taking total of player temp score to tempScore
     pushTemp(playerOne.playerTempScore);
-    // playerOne.takeScore(playerOne.playerTempScore);
+    //resets initial array playerPrevious
     playerOne.resetArray();
 
 
@@ -109,18 +101,17 @@ $(function(){
     $(".previous1Roll").text(tempScore);
     $(".current1Score").text(current);
   });
-// Click to Collect amount
+  // Click to Collect amount
   $("#stop1").click(function(){
     //this calculates finalScore and clears out tempScore
     // calculate(tempScore);
     calculate(tempScore);
-
+    //clears out tempScore
     tempScore = [];
+    //clears page and alerts you if you WIN!
     playerOne.win();
     playerTwo.win();
 
-
-   // var finalScore = this.finalScore + playerOne.playerTempScore;
     $("#player1Score").text(playerOne.finalScore);
     $(".current1Roll").text("");
     $(".previous1Roll").text("");
@@ -128,17 +119,17 @@ $(function(){
     $("#player1").hide();
     $("#player2").show();
   });
-//click to toll dice for playerTwo
+  //click to toll dice for playerTwo
   $("#roll2").click(function(){
 
-     arr = [1,2,3,4,5,6];
-     roll = arr[Math.floor(Math.random()*arr.length)];
+    arr = [1,2,3,4,5,6];
+    roll = arr[Math.floor(Math.random()*arr.length)];
     switchUser();
-bonusRound();
+    bonusRound();
     playerTwo.playerPrevious.push(roll)
     playerTwo.playerTempScore = 0;
     playerTwo.playerPrevious.forEach(function(currentScore){
-    playerTwo.playerTempScore += currentScore;
+      playerTwo.playerTempScore += currentScore;
     });
 
     var current = roll;
@@ -156,10 +147,6 @@ bonusRound();
   });
   //click to collect final score
   $("#stop2").click(function(){
-
-
-    // calculate(tempScore);
-//
     calculate2(tempScore);
     tempScore = [];
     playerOne.win()
@@ -172,7 +159,7 @@ bonusRound();
     $("#player2").hide();
     $("#player1").show();
     $(".round").text(round);
-  bonusText();
+    bonusText();
   });
 
 });
